e74b4d8a1da3b812e6ef2631a7cd25d8d30e2c4d
Bhuvana Sudheer <sudheerbhuvana25@gmail.com>
2026-08-07 11:27:33 +0530

    feat: add support for r2.klforge.in and update r2KeyFromUrl logic to handle multiple R2 domains

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'r2.klforge.in',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'pub-877712caba8544e39ecfeee23adcee20.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-6d49c8bed9a74670be47f74c46e5792e.r2.dev',
         port: '',
         pathname: '/**',
       },
